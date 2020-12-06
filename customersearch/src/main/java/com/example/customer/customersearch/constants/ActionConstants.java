0505860ccd80bc1dc5d71825e620b40befaf7b21
mohan5246 <mraj697@gmail.com>
2020-12-06 18:25:23 +0530

    Error Code Modified

--- customersearch/src/main/java/com/example/customer/customersearch/constants/ActionConstants.java
+++ customersearch/src/main/java/com/example/customer/customersearch/constants/ActionConstants.java
@@ -2,8 +2,8 @@ package com.example.customer.customersearch.constants;
 
 public interface ActionConstants {
 
-	static final String ERR_001 = "ERR_SQL_EXCEPTION";
-	static final String ERR_002 = "ERR_DATA_NOT_FOUND_EXCEPTION";
+	static final String ERR_001 = "ERR_DATA_NOT_FOUND_EXCEPTION";
+	static final String ERR_002 = "ERR_SQL_EXCEPTION";
 	static final String ERR_003 = "ERR_GENERIC_EXCEPTION";
 
 }
